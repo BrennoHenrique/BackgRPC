1f6bd4c645981d429cc36e43b287d2162e2e9458
BrennoHenrique <brennohh22@hotmail.com>
2022-03-01 17:23:23 -0300

    Initial commit

---
+++ Services/AuthService.cs
@@ -0,0 +1,19 @@
+﻿using BackgRPC.Protos;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace BackgRPC.Services
+{
+    public class AuthService : AuthProtoService.AuthProtoServiceBase
+    {
+        public override Task<AuthUserModel> AuthUser(AuthUserRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new AuthUserModel
+            {
+                Email = request.Email,
+                Token = Guid.NewGuid().ToString()
+            });
+        }
+    }
+}
